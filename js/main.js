c209a059fa9b92c3479dd0efef11d4848e9f1882
Odusanya <odusanya.tomi@gmail.com>
2017-03-31 17:18:04 +0100

    Finished App UI, Backend Javascript still incomplete

--- js/main.js
+++ js/main.js
@@ -17,13 +17,43 @@
 
 document.getElementById('input__btn').addEventListener('click', function() {
 	var value = document.getElementById('input_field').value;
-	if (value) {
-		console.log ('There is actually a value fam');
-	} else {
-		console.log ('No value fam');
-	}
+	if (value)	addItemTodo(value);
 	// console.log(value);
 });
 
 // button.addEventListener('click',vfunction, false);
 
+function addItemTodo (text) {
+
+	var list = document.getElementById('todo');
+
+	var item = document.createElement('li');
+	item.innerText = text;
+
+	var buttons = document.createElement('div');
+	buttons.classList.add('buttons');
+
+	var remove = document.createElement('button');
+	remove.classList.add('remove');
+	remove.innerHTML = removeSVG;
+
+	var complete = document.createElement('button');
+	complete.classList.add ('complete');
+	complete.innerHTML = completeSVG;
+
+	buttons.appendChild(remove);
+	buttons.appendChild(complete);
+	item.appendChild(buttons);
+
+	list.appendChild(item);
+}
+
+var tomi = {
+	name: 'Tomi Odusanya',
+	age: 'pushing 30',
+	address: 'Agbara'
+}
+
+tomi.name = 'TTT';
+
+console.log(tomi.name);
